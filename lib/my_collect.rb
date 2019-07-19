fc352b48a8d5e4d665187433c1ae2b263d860c6b
Tracie Masek <traciemasek@gmail.com>
2019-07-19 10:43:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(collection)
+  i = 0 
+  mod_collection = []
+  
+  while i < collection.length 
+    mod_collection.push(yield collection[i])
+    i += 1
+  end
+  mod_collection
+end
 
